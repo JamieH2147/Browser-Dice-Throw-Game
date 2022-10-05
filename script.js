e969e21098ad0693a9e354d80e6a1ef98263bfca
Jamie Hopwood <jamiehopwood@hotmail.co.uk>
2022-10-05 18:10:50 +0100

    fixed bug in double even throw calculation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if (dice1 === 6 && dice2 === 6) {
   alert( `You rolled ${dice1} & ${dice2}! You win a top prize: a bear!`)
 } else if (dice1 === dice2 && dice1 !==6 && dice2 !== 6) {
   alert(`You rolled ${dice1} & ${dice2}! You win three free throws!`)
-} else if (dice1 % 2 == 0 || dice2 % 2 == 0) {
+} else if (dice1 + dice2 % 2 === 0) {
   alert(`You rolled ${dice1} & ${dice2}! You win one free throw!`)
 } else {
   alert(`You rolled ${dice1} & ${dice2}! Sorry, you lose the game!`)
